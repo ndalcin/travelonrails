aee21e9fb1a8e91049c565a3b60c107045cd788c
ndalcin <dalcin.nicole@gmail.com>
2017-12-09 20:45:36 -0500

    vacation validations

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -5,8 +5,12 @@ class Vacation < ApplicationRecord
   belongs_to :user
   has_many :vacation_activities
   has_many :activities, through: :vacation_activities
-  #validates :name, presence: true
-
+  
+  validates :name, presence: true
+  validates :destination_id, presence: true
+  validates :budget, presence: true, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :length, presence: true, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :date, presence: true
 
 
   def activities_total
