136ea968a760bfc3ea1426cb5a5381ad5f8c170a
ndalcin <dalcin.nicole@gmail.com>
2017-12-10 19:17:41 -0500

    updates to nested form and more DRYing

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,8 +3,14 @@ class Activity < ApplicationRecord
   has_many :vacation_activities
   has_many :vacations, through: :vacation_activities
 
-  accepts_nested_attributes_for :vacation_activities
+  #accepts_nested_attributes_for :vacation_activities
 
+
+  def vacation_activities_attributes=(va_attrs)
+    va_attrs.values.each do |attribute|
+      self.vacation_activities.build(attribute)
+    end
+  end
 
   def self.top_five
     Activity.order('rating DESC').limit(5)
