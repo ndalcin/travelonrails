f1852ee57074ae17f06400d38610243b85bde526
ndalcin <dalcin.nicole@gmail.com>
2018-01-18 07:52:18 -0500

    added people to activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118025908) do
+ActiveRecord::Schema.define(version: 20180118125048) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180118025908) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.integer "people"
   end
 
   create_table "destinations", force: :cascade do |t|
